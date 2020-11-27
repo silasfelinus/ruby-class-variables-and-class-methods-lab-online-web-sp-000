178ce30567bf9babb3568d5263faf4d0c2231674
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-27 23:00:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,14 @@ def self.genres
   @@genres.uniq
 end
 
-
-
+def self.genre_count
+  genre_hash = {}
+  @@genres.each do |genre|
+    if genre_hash[genre]
+      genre_hash[genre] += 1
+    else
+      genre_hash[genre] = 1
+    end
+  end
+  genre_hash
 end
